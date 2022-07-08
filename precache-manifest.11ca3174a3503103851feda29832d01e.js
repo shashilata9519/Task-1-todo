7ac7c3092e11d502bb088fc0837ee5ef215475cb
Shashi Lata <100056409+shashilata9519@users.noreply.github.com>
2022-07-08 10:18:16 +0530

    Updates

---
+++ precache-manifest.11ca3174a3503103851feda29832d01e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8847432d359d09436966",
+    "url": "/Task-1-todo/static/css/main.9499c276.chunk.css"
+  },
+  {
+    "revision": "8847432d359d09436966",
+    "url": "/Task-1-todo/static/js/main.8847432d.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/Task-1-todo/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "d5d855b5f41283aa11b7",
+    "url": "/Task-1-todo/static/css/2.2df5b294.chunk.css"
+  },
+  {
+    "revision": "d5d855b5f41283aa11b7",
+    "url": "/Task-1-todo/static/js/2.d5d855b5.chunk.js"
+  },
+  {
+    "revision": "e815895eacd838a60ce4",
+    "url": "/Task-1-todo/static/js/runtime~main.e815895e.js"
+  },
+  {
+    "revision": "24acd18cc8ec9281c2ef4bd6b539c15b",
+    "url": "/Task-1-todo/static/media/list.24acd18c.png"
+  },
+  {
+    "revision": "30fc43d9f6e24940f030aa2f9b23d4b6",
+    "url": "/Task-1-todo/index.html"
+  }
+];
